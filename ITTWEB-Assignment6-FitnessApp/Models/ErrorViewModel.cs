dbe44853bc7f104d2b941c9fe01fe5b77dddb037
Jeppe Stærk <jeppe@staerk.io>
2017-11-28 21:48:14 +0100

    init

---
+++ ITTWEB-Assignment6-FitnessApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ITTWEB_Assignment6_FitnessApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
